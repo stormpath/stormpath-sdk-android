14e509e4eff809a44e70609a961f51ff2b98f8b7
Dino Kovač <dino.kovac@infinum.hr>
2016-02-22 13:53:46 +0100

    Remove unused methods

--- sdk/src/main/java/com/stormpath/sdk/models/RegisterParams.java
+++ sdk/src/main/java/com/stormpath/sdk/models/RegisterParams.java
@@ -32,44 +32,4 @@ public class RegisterParams implements Serializable {
         this.password = password;
         this.username = username;
     }
-
-    public String getGivenName() {
-        return givenName;
-    }
-
-    public void setGivenName(String givenName) {
-        this.givenName = givenName;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
