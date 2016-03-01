4c90198a79b25d6264d6abc7452c304f581dafc9
Dino Kovač <dino.kovac@infinum.hr>
2016-03-01 15:55:19 +0100

    Change deploy script to only upload sdk for now

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./gradlew bintrayUpload -Pbintray.user="$BINTRAY_USER" -Pbintray.apikey="$BINTRAY_API_KEY" -Pbintray.gpg.password="$BINTRAY_GPG_PASS"
+./gradlew :sdk:bintrayUpload -Pbintray.user="$BINTRAY_USER" -Pbintray.apikey="$BINTRAY_API_KEY" -Pbintray.gpg.password="$BINTRAY_GPG_PASS"
