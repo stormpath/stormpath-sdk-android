4ebcfe94c76bd6e8d1706d7a5c54ac40596b9613
Dino Kovač <dino.kovac@infinum.hr>
2016-03-09 14:16:48 +0100

    Fix lint issues, removed generated comments

--- sdk-ui/src/main/java/com/stormpath/sdk/ui/BaseFragment.java
+++ sdk-ui/src/main/java/com/stormpath/sdk/ui/BaseFragment.java
@@ -10,9 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class BaseFragment extends Fragment {
 
 
