8aa57e174b7166a5139896b6e7092e0c506eedb5
Dino Kovač <dino.kovac@infinum.hr>
2016-03-10 12:49:18 +0100

    Implement logout for social login example

--- StormpathSocialLoginSample/src/main/java/com/stormpath/sdk/exampleapp/SocialLoginActivity.java
+++ StormpathSocialLoginSample/src/main/java/com/stormpath/sdk/exampleapp/SocialLoginActivity.java
@@ -5,7 +5,6 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.stormpath.sdk.Stormpath;
 import com.stormpath.sdk.StormpathCallback;
 import com.stormpath.sdk.models.SocialProvidersResponse;
@@ -14,11 +13,15 @@ import com.stormpath.sdk.models.StormpathError;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class SocialLoginActivity extends AppCompatActivity implements FacebookCallback<LoginResult> {
 
     CallbackManager callbackManager;
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +30,35 @@ public class SocialLoginActivity extends AppCompatActivity implements FacebookCa
 
         // Don't forget to set your fb app id in strings.xml!
         callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        setUpButton(loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isUserLoggedIn()) {
+                    LoginManager.getInstance().logOut();
+                    Stormpath.logout();
+                    setUpButton(loginButton);
+                } else {
+                    LoginManager.getInstance().logInWithReadPermissions(SocialLoginActivity.this, null);
+                }
+            }
+        });
         LoginManager.getInstance().registerCallback(callbackManager, this);
+    }
+
+    private boolean isUserLoggedIn() {
+        return Stormpath.accessToken() != null;
+    }
+
+    private void setUpButton(Button loginButton) {
+        if (isUserLoggedIn()) {
+            loginButton.setText(R.string.log_out);
+        } else {
+            loginButton.setText(R.string.log_in_with_facebook);
+        }
     }
 
     @Override
@@ -38,7 +68,8 @@ public class SocialLoginActivity extends AppCompatActivity implements FacebookCa
                     @Override
                     public void onSuccess(Void aVoid) {
                         // we are logged in via fb!
-                        Toast.makeText(SocialLoginActivity.this, "Success! " + loginResult.getAccessToken().getToken(), Toast.LENGTH_LONG).show();
+                        Toast.makeText(SocialLoginActivity.this, "Successful login!", Toast.LENGTH_LONG).show();
+                        setUpButton(loginButton);
                     }
 
                     @Override
@@ -52,7 +83,6 @@ public class SocialLoginActivity extends AppCompatActivity implements FacebookCa
     public void onCancel() {
         // fb login was cancelled ny the user
         Toast.makeText(SocialLoginActivity.this, "Canceled!", Toast.LENGTH_LONG).show();
-
     }
 
     @Override
