27979ef79bbdc7c465a8550b85f7cd0badb6c1cc
Eric LW <eric@stormpath.com>
2016-03-23 16:16:37 -0700

    added additional script for sdk-ui

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 ./gradlew :sdk:bintrayUpload -Pbintray.user="$BINTRAY_USER" -Pbintray.apikey="$BINTRAY_API_KEY" -Pbintray.gpg.password="$BINTRAY_GPG_PASS"
+./gradlew :sdk-ui:bintrayUpload -Pbintray.user="$BINTRAY_USER" -Pbintray.apikey="$BINTRAY_API_KEY" -Pbintray.gpg.password="$BINTRAY_GPG_PASS"
