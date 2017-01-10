f40a6aa9d3d2b66190f64cce5333b7c7e266ca38
Edward Jiang <edward@stormpath.com>
2017-01-10 11:24:02 -0800

    added getters/setters to RegistrationForm

--- sdk/src/main/java/com/stormpath/sdk/models/RegistrationForm.java
+++ sdk/src/main/java/com/stormpath/sdk/models/RegistrationForm.java
@@ -3,9 +3,9 @@ package com.stormpath.sdk.models;
 import com.squareup.moshi.Json;
 
 import java.io.Serializable;
+import java.util.Map;
 
 public class RegistrationForm implements Serializable {
-
     @Json(name = "givenName")
     private String givenName;
 
@@ -21,15 +21,65 @@ public class RegistrationForm implements Serializable {
     @Json(name = "username")
     private String username;
 
-    public RegistrationForm(String givenName, String surname, String email, String password) {
-        this(givenName, surname, email, password, null);
-    }
+    @Json(name = "customData")
+    private Map<String, String> customData;
 
-    public RegistrationForm(String givenName, String surname, String email, String password, String username) {
-        this.givenName = givenName;
-        this.surname = surname;
+    public RegistrationForm(String email, String password) {
         this.email = email;
         this.password = password;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public RegistrationForm setGivenName(String givenName) {
+        this.givenName = givenName;
+        return this;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public RegistrationForm setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public RegistrationForm setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public RegistrationForm setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public RegistrationForm setUsername(String username) {
         this.username = username;
+        return this;
+    }
+
+    public Map<String, String> getCustomData() {
+        return customData;
+    }
+
+    public RegistrationForm setCustomData(Map<String, String> customData) {
+        this.customData = customData;
+        return this;
     }
 }
