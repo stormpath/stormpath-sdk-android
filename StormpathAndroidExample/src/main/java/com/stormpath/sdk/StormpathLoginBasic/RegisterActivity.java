4869ef596a1c2656a4e35848c22b1f1c20a593e0
Edward Jiang <edward@stormpath.com>
2017-01-10 11:24:57 -0800

    added registration

---
+++ StormpathAndroidExample/src/main/java/com/stormpath/sdk/StormpathLoginBasic/RegisterActivity.java
@@ -0,0 +1,56 @@
+package com.stormpath.sdk.StormpathLoginBasic;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.stormpath.sdk.Stormpath;
+import com.stormpath.sdk.StormpathCallback;
+import com.stormpath.sdk.models.RegistrationForm;
+import com.stormpath.sdk.models.StormpathError;
+
+/**
+ * Created by edjiang on 1/6/17.
+ */
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button registerButton;
+    private EditText firstNameInput;
+    private EditText lastNameInput;
+    private EditText emailInput;
+    private EditText passwordInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        registerButton = (Button) findViewById(R.id.button_register);
+        firstNameInput = (EditText) findViewById(R.id.input_firstName);
+        lastNameInput = (EditText) findViewById(R.id.input_lastName);
+        emailInput = (EditText) findViewById(R.id.input_email);
+        passwordInput = (EditText) findViewById(R.id.input_password);
+
+        registerButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        RegistrationForm registrationData = new RegistrationForm(emailInput.getText().toString(), passwordInput.getText().toString());
+        registrationData.setGivenName(firstNameInput.getText().toString())
+                .setSurname(lastNameInput.getText().toString());
+        Stormpath.register(registrationData, new StormpathCallback<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+
+                finish();
+            }
+
+            @Override
+            public void onFailure(StormpathError error) {
+                // TODO: display error dialog
+            }
+        });
+    }
+}
